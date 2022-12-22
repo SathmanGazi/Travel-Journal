b00be180f9ac79014adce1dfada29fcd3bf9cb41
Musa Al Sathman Gazi <gazi@usf.edu>
2022-12-22 10:11:12 -0500

    working?

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import data from './data';
+export default function App() {
+  const cardElement = data;
+}
---
+++ src/data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id:1,
+        title: "Venice",
+        location: "Italy",
+        map: "shorturl.at/aV259",
+        dateStart: "2 July 2018",
+        dateEnd: "13 July 2018",
+        description: "Italy? No? ok",
+        img:
+    },
+    {
+        id:2,
+        title: ""
+        location:
+        map:
+        dateStart:
+        dateEnd:
+        description:
+        img:
+    },
+    {
+        id:3,
+        title: ""
+        location:
+        map:
+        dateStart:
+        dateEnd:
+        description:
+        img:
+    },
+];
